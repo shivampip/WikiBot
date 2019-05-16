0ea87cc4d5853f82a8b4c424d21b945ed1a89b0f
shivampip <shivam301296@gmail.com>
2019-05-16 12:26:10 +0530

    'documentation

--- actions.py
+++ actions.py
@@ -4,15 +4,25 @@ from rasa_core_sdk.events import SlotSet
 import wikipedia 
 
 
+# There is a class for each custom action, This is for custom action 'action_wiki'
 class ActionWiki(Action):
+
+    # name method returns custom action name
     def name(self):
         return "action_wiki"
 
+    # Here, we write logic, external api calls and else
     def run(self, dispatcher, tracker, domain):
+
+        # Getting 'word' slot value
         word= tracker.get_slot("word")
-        print("###Word is {}".format(word)) 
+        
+        # Searching word on wikipedia and getting summary
         summary= wikipedia.summary(word) 
+
+        # Sending response back to user
         dispatcher.utter_message(summary)
+
         return []
 
 
@@ -22,6 +32,19 @@ class ActionServerRestart(Action):
         return "action_server_restart"
 
     def run(self, dispatcher, tracker, domain):
+
+        # Getting server_name slot value
         server= tracker.get_slot("server_name")
+        
         dispatcher.utter_message("Restarting {} server".format(server)) 
+
+        # Logic to restart given server
+        #
+        #
+        #
+
+        # Acknoledgement
+        dispatcher.utter_message("Server restarted successfully")
+        #dispatcher.utter_message("Server couldn't be restarted")
+
         return []
